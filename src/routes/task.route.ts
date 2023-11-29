2dd6e1ef754f4a2555757e65f0993108034a2fc7
mtrnik <martin@oktodigital.com>
2023-11-29 11:14:36 +0100

    express validator

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import { Task } from '../models/task.interface';
 
 const router = Router();
 let tasks: Task[] = [];
 
+const taskValidationRules = [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    body('completed').isBoolean().withMessage('Completed must be a boolean'),
+];
 
 router.get('/', (req: Request, res: Response) => {
     res.json(tasks);
@@ -20,19 +26,25 @@ router.get('/:id', (req: Request, res: Response) => {
 });
 
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', taskValidationRules, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const task: Task = {
         id: tasks.length + 1,
         title: req.body.title,
         description: req.body.description,
-        completed: false,
+        completed: req.body.completed,
     };
 
     tasks.push(task);
     res.status(201).json(task);
 });
 
-router.put('/:id', (req: Request, res: Response) => {
+router.put('/:id',  taskValidationRules, (req: Request, res: Response) => {
     const task = tasks.find((t) => t.id === parseInt(req.params.id));
 
     if (!task) {
