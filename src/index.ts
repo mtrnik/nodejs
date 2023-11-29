547d2c131a81915e1a71810edb3eb741094085ba
mtrnik <martin@oktodigital.com>
2023-11-29 11:02:42 +0100

    task interface and routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response , Application } from 'express';
 
+import taskRoutes from './routes/task.route';
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+
+app.use('/tasks', taskRoutes);
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world in typescript');
+    res.send('Hello world in typescript 2');
 });
 
 app.listen(port, () => {
